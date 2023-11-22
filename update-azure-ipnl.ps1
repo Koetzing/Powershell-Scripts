d9e8d2b684e5d8b03be8fcde1ba100e960ffcc74
Thomas Kötzing <Thomas@koetzingit.de>
2023-11-22 11:23:48 +0100

    Create update-azure-ipnl.ps1

---
+++ update-azure-ipnl.ps1
@@ -0,0 +1,51 @@
+    <#
+        .SYNOPSIS
+            Updates Azure IP Named Locations with the current external dynamic IP-adress
+        
+        .LINKS
+            https://www.koetzingit.de        
+                
+        .NOTES
+        - Fill out the required parameters with SecPWD, TenantID, AppID and IPNamedLocation.
+
+    #>
+#
+# Powershell Azure Login
+#
+$SecurePassword = ConvertTo-SecureString -String "<SecPWD>" -AsPlainText -Force
+$TenantId = '<TenantID>'
+$ApplicationId = '<AppID>'
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecurePassword
+Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential | out-null
+
+#
+# Get the current publich ip address
+#
+$CurrentPubIP = (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+
+#
+# Connect to MS Graph
+#
+Connect-MgGraph -Scopes 'Policy.ReadWrite.ConditionalAccess' -NoWelcome | out-null
+
+#
+# Set IPLocation with current IP
+#
+$body = @{
+	"@odata.type" = "#microsoft.graph.ipNamedLocation"
+	displayName = "<IPNamedLocation>"
+	isTrusted = $true
+	ipRanges = @(
+		@{
+			"@odata.type" = "#microsoft.graph.iPv4CidrRange"
+			cidrAddress = "$CurrentPubIP/32"
+		}
+	)
+}
+
+#
+# Update the IPLocation
+#
+Update-MgIdentityConditionalAccessNamedLocation -NamedLocationId '018026cc-47f6-4faf-aefd-2436702a41a2' -BodyParameter $body
+
+Write-Host "New IP address for location Hetzles: $CurrentPubIP"
